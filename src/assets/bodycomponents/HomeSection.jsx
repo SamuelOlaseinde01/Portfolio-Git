6a02b76bb7a7420f99da331b9bd6bd7bea694158
Samuel <samuelolaseinde14@gmail.com>
2024-05-31 01:38:51 +0100

    l

--- src/assets/bodycomponents/HomeSection.jsx
+++ src/assets/bodycomponents/HomeSection.jsx
@@ -6,7 +6,7 @@ function HomeSection(props) {
     <section className="section-home" id="section-home">
     <div className={props.hamburger ? "container" : "container inner"}>
         <div className="h1andp">
-            <img src="smilingman.jpeg" alt="" srcset="" className="smiling-img"/>
+            <img src="src/images/smilingman.jpeg" alt="" srcset="" className="smiling-img"/>
             <h1>Hello</h1>
             <p>My name is Samuel, I am a Web developer</p>
         </div>
